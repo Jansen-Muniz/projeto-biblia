433593dcab6dfbbb0a80a44b06bf80ebcd0d8358
Jansen-Muniz <jansen.muniz1@gmail.com>
2023-01-30 22:24:27 -0300

    first commit

---
+++ app.js
@@ -0,0 +1,24 @@
+//'https://www.abibliadigital.com.br/api/verses/ra/sl/23'
+
+ const versesContainer = document.querySelector('#verses-container')
+ const bookNameBible = document.querySelector('#book-name')
+ const chapterBibleNumber = document.querySelector('#chapter')
+
+ const getBook = async (bookName, chapter) => {
+   const response = await fetch(`https://www.abibliadigital.com.br/api/verses/ra/${bookName}/${chapter}`)
+   return await response.json()
+ }
+
+ const addVersesIntoDOM = async (bookName, chapterNumber) => {
+   const { book, chapter, verses } = await getBook(bookName, chapterNumber)
+   const arrayVerses = verses
+   const versesTemplate = arrayVerses
+       .map(item =>
+         `<p><span>${item.number} </span>${item.text}</p>`).join('')
+  
+     bookNameBible.textContent = book.name
+     chapterBibleNumber.textContent = chapter.number
+     versesContainer.innerHTML += versesTemplate
+ }
+
+ addVersesIntoDOM('ex', '1')
