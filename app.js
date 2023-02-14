219ee645c19e10893d517cdab1c6e871dc66b576
Jansen-Muniz <jansen.muniz1@gmail.com>
2023-02-14 09:50:33 -0300

    novas alterações index, style e app

--- app.js
+++ app.js
@@ -1,47 +1,54 @@
-//'https://www.abibliadigital.com.br/api/verses/ra/sl/23'
-
  const versesContainer = document.querySelector('#verses-container')
  const bookNameBible = document.querySelector('#book-name')
  const chapterBibleNumber = document.querySelector('#chapter')
  const formSearch = document.querySelector('[data-js="form-search"]') 
+ const clearButton = document.querySelector('[data-js="clear-button"]')
+ const backToTopButton = document.querySelector('[data-js="back-to-top-button"]')
 
- const getBook = async (bookName, chapter) => {
+ const getUrl = (bookName, chapter) => `https://www.abibliadigital.com.br/api/verses/ra/${bookName}/${chapter}`
+
+ const fetchBook = async (bookName, chapter) => {
   try{
-    const response = await fetch(`https://www.abibliadigital.com.br/api/verses/ra/${bookName}/${chapter}`)
+    const response = await fetch(getUrl(bookName, chapter))
     
     if(!response.ok){
       throw new Error('Não foi possível obter os dados.')
     }
     
-    return response.json()
+    const bookData = await response.json()
+    return bookData
   } catch(error){
     alert(error.message)
   }
  }
-
- const getVerses = async (bookName, chapterNumber) => {
-   const { book, chapter, verses } = await getBook(bookName, chapterNumber)
-   const arrayVerses = verses
-   const versesTemplate = arrayVerses
-       .map(item =>
-         `<p><span>${item.number} </span>${item.text}</p>`).join('')
-         
-         bookNameBible.textContent = book.name
-         chapterBibleNumber.textContent = `Capítulo ${chapter.number}`
-         versesContainer.innerHTML += versesTemplate
- }
  
-  formSearch.addEventListener('submit', e => {
-    e.preventDefault()
+formSearch.addEventListener('submit', async e => {
+  e.preventDefault()
+  
+  const inputBookValue = e.target.book.value
+  const inputChapterValue = e.target.chapter.value
+  
+  const { book, chapter, verses } = await fetchBook(inputBookValue, inputChapterValue)
 
-    const inputBookValue = e.target.book.value
-    const inputChapterValue = e.target.chapter.value
+   const versesTemplate = verses.map(item => `
+     <p><span>${item.number}</span>${item.text}</p>
+     `).join('')
+   
+   bookNameBible.textContent = book.name
+   chapterBibleNumber.textContent = `${chapter.number}`
+   versesContainer.innerHTML +=  versesTemplate
 
-    getVerses(inputBookValue, inputChapterValue)
+   clearButton.classList.remove('hidden')
+   clearButton.classList.add('show')
+   
+   formSearch.reset()
+   e.target.book.focus()
+})
 
-    formSearch.reset()
-    e.target.book.focus()
-    scrollTo(0, 1000)
-  })
-
+clearButton.addEventListener('click', () => {
+  bookNameBible.textContent = ''
+  chapterBibleNumber.textContent = ''
+  versesContainer.textContent = ''
+  clearButton.classList.add('hidden')  
+})
 
