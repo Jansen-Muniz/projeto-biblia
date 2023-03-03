0aff788c156bc5411d7ab6ffbcc84db9eba64782
Jansen-Muniz <jansen.muniz1@gmail.com>
2023-03-03 08:25:21 -0300

    novas alterações

--- app.js
+++ app.js
@@ -1,54 +1,39 @@
- const versesContainer = document.querySelector('#verses-container')
- const bookNameBible = document.querySelector('#book-name')
- const chapterBibleNumber = document.querySelector('#chapter')
- const formSearch = document.querySelector('[data-js="form-search"]') 
- const clearButton = document.querySelector('[data-js="clear-button"]')
- const backToTopButton = document.querySelector('[data-js="back-to-top-button"]')
+ const versesContainer = document.querySelector('[data-js="verses-container"]')
+ const bookNameBible = document.querySelector('[data-js="book-name"]')
+ const chapterBibleNumber = document.querySelector('[data-js="chapter"]')
+ const formSearchBook = document.querySelector('[data-js="form-search"]') 
+ const inputBook = document.querySelector('[data-js="input-book"]')
+ const inputChapter = document.querySelector('[data-js="input-chapter"]')
+ const bookAuthor = document.querySelector('[data-js="book-author"]')
+ const bookVersion = document.querySelector('[data-js="book-version"]')
+ const bookGroup = document.querySelector('[data-js="book-group"]')
 
- const getUrl = (bookName, chapter) => `https://www.abibliadigital.com.br/api/verses/ra/${bookName}/${chapter}`
+ const getUrl = (book, chapter) => `https://www.abibliadigital.com.br/api/verses/ra/${book}/${chapter}`
 
- const fetchBook = async (bookName, chapter) => {
-  try{
-    const response = await fetch(getUrl(bookName, chapter))
-    
-    if(!response.ok){
-      throw new Error('Não foi possível obter os dados.')
+ const fetchBook = async url => {
+    try{
+      const response = await fetch(url)
+
+      if(!response.ok){
+        throw new Error('Não foi possível obter as informações')
+      }
+
+      const bookData = await response.json()
+      return bookData
+    }catch(err){
+      alert(err.message)
     }
-    
-    const bookData = await response.json()
-    return bookData
-  } catch(error){
-    alert(error.message)
-  }
  }
- 
-formSearch.addEventListener('submit', async e => {
-  e.preventDefault()
-  
-  const inputBookValue = e.target.book.value
-  const inputChapterValue = e.target.chapter.value
-  
-  const { book, chapter, verses } = await fetchBook(inputBookValue, inputChapterValue)
 
-   const versesTemplate = verses.map(item => `
-     <p><span>${item.number}</span>${item.text}</p>
-     `).join('')
-   
-   bookNameBible.textContent = book.name
-   chapterBibleNumber.textContent = `${chapter.number}`
-   versesContainer.innerHTML +=  versesTemplate
+ formSearchBook.addEventListener('submit', async e => {
+    e.preventDefault()
+    const {book, chapter, verses} = await fetchBook(getUrl(e.target.book.value, e.target.chapter.value))
 
-   clearButton.classList.remove('hidden')
-   clearButton.classList.add('show')
-   
-   formSearch.reset()
-   e.target.book.focus()
+    bookAuthor.textContent = `Autor: ${book.author}`
+    bookVersion.textContent = `Versão: ${book.version}`
+    bookGroup.textContent = `Grupo: ${book.group}`
+    bookNameBible.textContent = book.name
+    chapterBibleNumber.textContent = chapter.number
+    
+    versesContainer.textContent = verses.map(verse => `${verse.text}`).join(' ')
 })
-
-clearButton.addEventListener('click', () => {
-  bookNameBible.textContent = ''
-  chapterBibleNumber.textContent = ''
-  versesContainer.textContent = ''
-  clearButton.classList.add('hidden')  
-})
-
